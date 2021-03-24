40570881cb6083970c922371d72f0051fd5d838c
Alexandr Sivohin <alexandrsivoh@gmail.com>
2021-03-24 05:37:17 +0300

    v 1.0

---
+++ ExceptionTask1/MyExcwption.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExceptionTask1
+{
+    public class MyException : Exception
+    {
+        public MyException()
+        {
+            Console.Write("\t Oшибка: ");
+        }
+        public MyException(string message) : base(message)
+        { }
+
+    }
+}
--- ExceptionTask1/Program.cs
+++ ExceptionTask1/Program.cs
@@ -6,7 +6,48 @@ namespace ExceptionTask1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("программва запущена\n");
+            MyException my = new MyException("Cлучилось страшное! Но мы уже знаем об этом");
+            
+            var uri = new UriFormatException(); 
+            var noMethod = new NotSupportedException();
+            var exMethod = new InvalidOperationException();
+            var noIndex = new IndexOutOfRangeException();
+            Exception[] array = new Exception[] { my, uri, noMethod, exMethod, noIndex };
+            foreach (Exception i in array)
+            {
+                try
+                { 
+                    MyException myVoid = new MyException();
+                    throw i;
+                    
+                }
+                catch (MyException mex)
+                {
+                    Console.WriteLine(mex.Message);
+                }
+                catch (Exception ex) when (ex is UriFormatException)
+                {
+                    Console.WriteLine("Используется недопустимый универсальный код ресурса(URI)");
+                }
+                catch (Exception ex) when (ex is NotSupportedException)
+                {
+                    Console.WriteLine("етод или операция не поддерживается");
+                }
+                catch (Exception ex) when (ex is InvalidOperationException)
+                {
+                    Console.WriteLine("Вызов метода недопустим в текущем состоянии объекта");
+                }
+                catch (Exception ex) when (ex is IndexOutOfRangeException)
+                {
+                    Console.WriteLine("Индекс находится за пределами границ массива или коллекции");
+                }
+
+            }
+            Console.WriteLine("\nпрограмма закончила работу");
+            Console.ReadKey();
         }
     }
+
 }
